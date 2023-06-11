ab03121c612d00fa094f29b269d42e7815b246e7
syk <yeonkyung.son@vitruv.co.kr>
2023-06-12 00:14:54 +0900

    test

---
+++ src/test/java/com/project/ppoba/PpobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ppoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
